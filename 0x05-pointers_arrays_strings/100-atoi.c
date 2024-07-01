1bbef996c9e8d428b9909b7ebd8351d705793086
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-07-01 16:42:10 +0300

    added 100-atoi.c, 100-main.c, modified main.h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -8,7 +9,8 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1;
+	unsigned int result = 0;
 
 	while (s[i] != '\0')
 	{
@@ -18,6 +20,15 @@ int _atoi(char *s)
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (result > (unsigned int)(INT_MAX / 10) ||
+			    (result == (unsigned int)(INT_MAX / 10) &&
+			    (s[i] - '0') > (INT_MAX % 10)))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (result != 0)
